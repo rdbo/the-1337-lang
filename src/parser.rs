68441aaf377eaca9935044d9f5b433cfacaeaedf
rdbo <rdbodev@gmail.com>
2025-08-26 10:27:22 +0000

    refactor parser

--- src/parser.rs
+++ src/parser.rs
@@ -11,25 +11,6 @@ pub struct NodeInfo {
     pub tokens: Vec<TokenInfo>,
 }
 
-macro_rules! let_variant {
-    ($token_variant:ident, $ident:ident, $token_info_option_expr:expr) => {
-        let Some(Token::$token_variant($ident)) =
-            $token_info_option_expr.and_then(|x| Some(x.token.clone()))
-        else {
-            return Node::Invalid;
-        };
-    };
-}
-
-macro_rules! assure_variant {
-    ($token_variant:ident, $token_info_option_expr:expr) => {
-        let Some(Token::$token_variant) = $token_info_option_expr.and_then(|x| Some(&x.token))
-        else {
-            return Node::Invalid;
-        };
-    };
-}
-
 impl Parser {
     pub fn new(content: String) -> Self {
         Self {
@@ -45,27 +26,35 @@ impl Parser {
         last_depth.last()
     }
 
+    fn next_token(&mut self) -> Option<&Token> {
+        self.next_token_info().and_then(|x| Some(&x.token))
+    }
+
+    fn next_token_owned(&mut self) -> Option<Token> {
+        self.next_token().and_then(|x| Some(x.to_owned()))
+    }
+
     fn parse_function_type(&mut self) -> Option<Type> {
         let mut params: Vec<(String, Type)> = Vec::new();
         loop {
             // TODO: Deny right parenthesis after comma
-            let token_info = self.next_token_info()?.to_owned();
-            if let Token::RightParen = token_info.token {
+            let token = self.next_token()?.to_owned();
+            if matches!(token, Token::RightParen) {
                 break;
             }
 
-            let Token::Identifier(identifier) = &token_info.token else {
+            let Token::Identifier(identifier) = &token else {
                 return None;
             };
-            let Token::Colon = self.next_token_info()?.token else {
+            let Token::Colon = self.next_token()? else {
                 return None;
             };
             let param_type = self.parse_type()?;
 
             params.push((identifier.to_owned(), param_type));
 
-            let token_info = self.next_token_info()?;
-            match token_info.token {
+            let token = self.next_token()?;
+            match token {
                 Token::Comma => continue,
                 Token::RightParen => break,
                 _ => return None,
@@ -82,8 +71,8 @@ impl Parser {
     }
 
     fn parse_type(&mut self) -> Option<Type> {
-        let token_info = self.next_token_info()?;
-        let t = match &token_info.token {
+        let token = self.next_token()?;
+        let t = match token {
             Token::Identifier(s) => Type::Common(s.to_owned()),
             Token::Times => Type::Pointer(Box::new(self.parse_type()?)),
             Token::LeftParen => self.parse_function_type()?,
@@ -94,8 +83,12 @@ impl Parser {
     }
 
     pub fn parse_extern(&mut self) -> Node {
-        let_variant!(Identifier, identifier, self.next_token_info());
-        assure_variant!(Colon, self.next_token_info());
+        let Some(Token::Identifier(identifier)) = self.next_token_owned() else {
+            return Node::Invalid;
+        };
+        let Some(Token::Colon) = self.next_token() else {
+            return Node::Invalid;
+        };
         let Some(declared_type) = self.parse_type() else {
             return Node::Invalid;
         };
@@ -110,8 +103,8 @@ impl Parser {
         // Push new parsing depth for this (sub)node
         self.pending_tokens.push(vec![]);
 
-        let next_token = self.next_token_info()?;
-        let node = match next_token.token {
+        let next_token = self.next_token()?;
+        let node = match next_token {
             Token::KwExtern => self.parse_extern(),
             _ => Node::Invalid,
         };
